dc5401823486bee8497ed901b8deaffd90dafe00
James Cook <jamesatjaminit+github@jaminit.co.uk>
2022-09-20 13:10:52 +0000

    fix: stupid ts

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={0}>
       <div>
