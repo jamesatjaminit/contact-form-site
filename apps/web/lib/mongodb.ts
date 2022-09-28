4691845e3a07612c348ea715e0d2dad80b570c26
James Cook <james@jaminit.co.uk>
2022-09-28 17:09:48 +0100

    fix: everythign

--- apps/web/lib/mongodb.ts
+++ apps/web/lib/mongodb.ts
@@ -3,15 +3,14 @@ import { MongoClient } from "mongodb";
 
 const uri = String(process.env.MONGODB_URI);
 const options = {};
-
 let client;
 let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
+  throw new Error("No MongoDB URI found.");
 }
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV == "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   // @ts-expect-error
