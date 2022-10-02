950ce1aa87030482decfd4f76c0d140a728229cb
James Cook <james@jaminit.co.uk>
2022-10-02 11:53:42 +0100

    feat: noindex

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,15 +2,15 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }: any) {
   return (
     <SessionProvider session={pageProps.session}>
-      <div>
-        <Component {...pageProps} />
-      </div>
+      <DefaultSeo dangerouslySetAllPagesToNoIndex />
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
